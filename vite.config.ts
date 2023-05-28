ef18aeff63494a006ec8af40b6b0802298e49037
arkhlima <arkhlima@gmail.com>
2023-05-28 16:25:21 +0700

    build: changed my mind — use vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import solid from 'solid-start/vite'
-import cloudflare from 'solid-start-cloudflare-pages'
+import vercel from 'solid-start-vercel'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [solid({ ssr: false, adapter: cloudflare({}) })],
+  plugins: [solid({ adapter: vercel(), ssr: false })],
 })
